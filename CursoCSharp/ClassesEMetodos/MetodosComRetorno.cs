3b8ff2c1f5f2856c2f7128e8244fc8d146db85ab
nilwandjg <nilwandjg@yahoo.com.br>
2024-02-02 06:18:32 -0300

    Métodos com retorno #1

---
+++ CursoCSharp/ClassesEMetodos/MetodosComRetorno.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class CalculadoraComum
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtrair(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Dividir(int a, int b)
+        {
+            return a / b;
+        }
+    }
+
+    class MetodosComRetorno
+    {
+        public static void Executar()
+        {
+            var calculadoraComum = new CalculadoraComum();
+            var resultado = calculadoraComum.Somar(5, 5);
+            Console.WriteLine(resultado);
+            Console.WriteLine(calculadoraComum.Subtrair(2, 7));
+            Console.WriteLine(calculadoraComum.Multiplicar(4, 4));
+        }
+    }
+}
