ccc154959e0b7293495045898801a38f89a8234d
nilwandjg <nilwandjg@yahoo.com.br>
2024-02-14 03:15:20 -0300

    Propriedades (props)

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -46,6 +46,7 @@ namespace CursoCSharp {
                 {"Params - Classes e Métodos", Params.Executar},
                 {"Parametros Nomeados - Classes e Métodos", ParametrosNomeados.Executar},
                 {"Get Set - Classes e Métodos", GetSet.Executar},
+                {"Props - Classes e Métodos", Props.Executar},
             });
 
             central.SelecionarEExecutar();
