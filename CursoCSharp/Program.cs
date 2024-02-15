d9a2ee86e7a3cb6786c13d1aab683aa4ee2d9b04
nilwandjg <nilwandjg@yahoo.com.br>
2024-02-14 21:05:51 -0300

    Atributos readonly

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -47,6 +47,7 @@ namespace CursoCSharp {
                 {"Parametros Nomeados - Classes e Métodos", ParametrosNomeados.Executar},
                 {"Get Set - Classes e Métodos", GetSet.Executar},
                 {"Props - Classes e Métodos", Props.Executar},
+                {"Readonly - Classes e Métodos", Readonly.Executar},
             });
 
             central.SelecionarEExecutar();
