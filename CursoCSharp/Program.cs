f29c9d5649b2bcb1c398fb34d4930745e687fc37
nilwandjg <nilwandjg@yahoo.com.br>
2024-03-01 04:41:31 -0300

    Herança - parte 1

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -5,6 +5,7 @@ using CursoCSharp.Fundamentos;
 using CursoCSharp.EstruturaDeControle;
 using CursoCSharp.ClassesEMetodos;
 using CursoCSharp.Colecoes;
+using CursoCSharp.OO;
 
 namespace CursoCSharp {
     class Program {
@@ -64,6 +65,8 @@ namespace CursoCSharp {
                 {"Igualdade - Coleções", Igualdade.Executar},
                 {"Stack - Coleções", ColecoesStack.Executar},
                 {"Dictionary - Coleções", ColecoesDictionary.Executar},
+                //Orientação a Objetos
+                {"Herança - OO", Heranca.Executar},
             });
 
             central.SelecionarEExecutar();
